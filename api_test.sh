b140f30f7ed9c2a8485e1af2fec59416e2bed433
tierneyj <tierneyj@bu.edu>
2015-11-10 16:55:33 -0500

    Add PUT/PUSH methods

--- api_test.sh
+++ api_test.sh
@@ -10,6 +10,8 @@ return_all(){
 	echo "Checking 'return all' functionality..."
 	echo "curl -i $WEBPATH"
 	echo "..."
+	echo 
+	echo "Response from server..."
 	curl -i $WEBPATH
 	echo
 }
@@ -21,6 +23,8 @@ return_one(){
 	echo "Returning patient record...$key"
  	echo "curl -i $WEBPATH/$key"
 	echo "..."
+	echo 
+	echo "Response from server..."
 	curl -i $WEBPATH/$key
 	echo
 }
@@ -36,13 +40,59 @@ create_record(){
 	read -p "Enter sex: " sex
 	echo "Creating patient record..."
 	echo "curl -i -H \"Content-Type: application/json\" -X POST -d '{\"firstName\":\"$firstName\", \"lastName\":\"$lastName\", \"dob\":\"$dob\", \"height\":\"$height\", \"weight\":\"$weight\", \"sex\":\"$sex\"}' $WEBPATH"
+	echo "..."
+	echo
+	echo "Response from server..."
 	curl -i -H "Content-Type: application/json" -X POST -d '{"firstName": "'$firstName'", "lastName":"'$lastName'", "dob":"'$dob'", "height":'$height', "weight":'$weight', "sex":"'$sex'"}' $WEBPATH
-
+	echo
 }
 
 update_record(){
+	echo "Checking 'create record' functionality..."
+	read -n1 -p "Enter Patient ID to update..." key
 	echo
-}
+	echo "Enter values to update fields. If you do not wish to update, hit enter."
+	read -p "Enter first name : " firstName
+	read -p "Enter last name : " lastName
+	read -p "Enter date of birth: " dob
+	read -p "Enter height: " height
+	read -p "Enter weight: " weight
+	read -p "Enter sex: " sex
+	echo "Updating patient record $key..."
+	json=""
+
+	if [ "$firstName" != "" ]; then
+		json=''$json'"firstName":"'$firstName'",'
+	fi
+	if [ "$lastName" != "" ]; then
+		json=''$json'"lastName":"'$lastName'",'
+	fi
+	if [ "$dob" != "" ]; then
+		json=''$json'"dob":"'$dob'",'
+	fi
+	if [ "$height" != "" ]; then
+		json=''$json'"height":'$height','
+	fi
+	if [ "$weight" != "" ]; then
+		json=''$json'"weight":'$weight','
+	fi
+	if [ "$sex" != "" ]; then
+		json=''$json'"sex":"'$sex'",'
+	fi
+
+	if [ "$json" == "" ]; then
+		echo "Nothing to update"
+	else
+		json="${json%?}"
+		echo "curl -i -H \"Content-Type: application/json\" -X PUT -d '{$json}' $WEBPATH/$key"
+		echo "..."
+		echo
+		echo "Response from server..."
+		curl -i -H "Content-Type: application/json" -X PUT -d '{'$json'}' $WEBPATH/$key
+		echo
+	fi
+
+	}
 
 delete_record(){
 	echo "Checking 'delete record' functionality..."
@@ -51,6 +101,8 @@ delete_record(){
 	echo "Deleting patient record...$key"
 	echo "curl -X DELETE $WEBPATH/$key"
 	echo "..."
+	echo 
+	echo "Response from server..."
 	curl -X DELETE $WEBPATH/$key
 	echo
 }
@@ -69,8 +121,17 @@ print_table(){
 get_response(){
 	read -n1 -r -p "Enter test number to run..." key
 	echo
+	if [ "$key" == "" ]
+	then
+		echo "Please enter a number"
+		sleep .5
+		print_table
+		get_response
+	fi
 	echo "Running test $key..."
 	sleep .5
+
+	
 
 	if [ "$key" -eq "1" ]
 	then
@@ -81,6 +142,9 @@ get_response(){
 	elif [ "$key" -eq "3" ]
 	then 
 		create_record
+	elif [ "$key" -eq "4" ]
+	then 
+		update_record
 	elif [ "$key" -eq "5" ]
 	then
 		delete_record
