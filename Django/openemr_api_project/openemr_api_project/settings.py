2ac37783a151766ee88181b8a514b84a1c6ede31
ryanoflaherty <ryflare@bu.edu>
2016-01-31 17:42:34 -0500

    Requirements + DB
    Added a requirements.txt and upodated database settings.

--- Django/openemr_api_project/openemr_api_project/settings.py
+++ Django/openemr_api_project/openemr_api_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'openemr_api_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'openemr',
+	'USER': 'API_server',
+	'PASSWORD': 'django',
+	'HOST': 'localhost',
+	'PORT': '3306',
     }
 }
 
