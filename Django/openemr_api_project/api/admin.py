8f84e182e126ecd4130ab982ff785b8aedfbb652
Ryan O'Flaherty <ryflare@bu.edu>
2016-02-05 15:51:21 -0500

    Remove 'Users' model from Django
    We removed the Users model from Django because we decided all auth will go through the Django app for access to the OpenEMR Database.  Users of the mobile app will need to be configured through the admin portal hosted by Django and not OpenEMR. It is an important design decision that these be kept separate.

--- Django/openemr_api_project/api/admin.py
+++ Django/openemr_api_project/api/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from api.models import Users, PatientData, HistoryData, InsuranceData
+from api.models import PatientData, HistoryData, InsuranceData
 # Register your models here.
 
-admin.site.register(Users)
 admin.site.register(PatientData)
 admin.site.register(HistoryData)
 admin.site.register(InsuranceData)
