1396ae7b66341bdd2e83e31eb62b5325150a38f8
root <root@ip-172-31-43-22.us-west-2.compute.internal>
2016-02-28 13:13:12 -0500

    commit changes

--- Django/openemr_api_project/openemr_api_project/settings.py
+++ Django/openemr_api_project/openemr_api_project/settings.py
@@ -138,6 +138,7 @@ STATICFILES_DIRS = (
 REST_FRAMEWORK = {
 	'DEFAULT_AUTHENTICATION_CLASSES': (
 		'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
 	),
 	'DEFAULT_PERMISSION_CLASSES': (
 		'rest_framework.permissions.IsAuthenticatedOrReadOnly',
