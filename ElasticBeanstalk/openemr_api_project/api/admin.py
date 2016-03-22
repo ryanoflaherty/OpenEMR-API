a779129c6e9812ba875b181a34d4279d9b727903
minggLu <mylu@bu.edu>
2016-03-21 22:27:35 -0400

    I don't even know anymore hope this doesn't break shit

---
+++ ElasticBeanstalk/openemr_api_project/api/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from api.models import PatientData, HistoryData, FormEncounter, IssueEncounter, Forms, Lists, ListsTouch, Facility, FormVitals, FormRos, FormReviewofs
+# Register your models here.
+
+admin.site.register(PatientData)
+admin.site.register(HistoryData)
+admin.site.register(FormEncounter)
+admin.site.register(IssueEncounter)
+admin.site.register(Forms)
+admin.site.register(Lists)
+admin.site.register(ListsTouch)
+admin.site.register(Facility)
+admin.site.register(FormVitals)
+admin.site.register(FormRos)
+admin.site.register(FormReviewofs)
--- ElasticBeanstalk/openemr_api_project/openemr_api_project/wsgi.py
+++ ElasticBeanstalk/openemr_api_project/openemr_api_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for openemr_api_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+import django.core.handlers.wsgi 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/ec2-user/OpenEMR-API/Django/openemr_api_project')
+sys.path.append('/home/ec2-user/OpenEMR-API/Django/openemr_api_project/openemr_api_project')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openemr_api_project.settings")
 
 application = get_wsgi_application()
+#application = django.core.handlers.wsgi.WSGIHandler()
