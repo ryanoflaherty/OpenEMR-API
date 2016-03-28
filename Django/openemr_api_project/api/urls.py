c1c1761de07363a940bdc4ce462414680905a065
ryanoflaherty <ryflare@bu.edu>
2016-03-28 19:42:10 -0400

    brand browsable api, add new api root url

--- Django/openemr_api_project/api/urls.py
+++ Django/openemr_api_project/api/urls.py
@@ -7,7 +7,7 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 #router.register(r'records/patient-data', views.PatientDataViewSet)
-router.register(r'records/history-data', views.HistoryDataViewSet)
+#router.register(r'records/history-data', views.HistoryDataViewSet)
 router.register(r'records/medical-history', views.MedicalHistoryViewSet)
 
 
@@ -15,7 +15,8 @@ router.register(r'records/medical-history', views.MedicalHistoryViewSet)
 API Endpoints for authentication
 """
 urlpatterns = [
-    url(r'^', include(router.urls)),  # API Routing
+    url(r'^viewsets/', include(router.urls)),  # API Routing
+    url(r'^$', views.api_root),
     url(r'^token/$', rest_views.obtain_auth_token),  # POST to get token
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),  # Authenticate to API
 ]
@@ -26,6 +27,6 @@ API Endpoints for app functionality
 
 
 urlpatterns += [
-    url(r'^records/(?P<pid>[0-9]+)/history-data', views.HistoryDataList.as_view()),  # Return a list of visits based on pid
-    url(r'^records/patient-data', views.PatientDataList.as_view())
+    url(r'^records/(?P<pid>[0-9]+)/history-data', views.HistoryDataList.as_view(), name='history-data-list'),  # Return a list of visits based on pid
+    url(r'^records/patient-data', views.PatientDataList.as_view(), name='patient-data-list'),
 ]
--- Django/openemr_api_project/api/views.py
+++ Django/openemr_api_project/api/views.py
@@ -6,11 +6,68 @@ from api.serializers import UserSerializer, GroupSerializer, PatientDataSerializ
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rest_framework import filters, generics
-from rest_framework import authentication, permissions
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+
+
+"""
+This view returns the homepage to the user.
+"""
 
 
 def index(request):
 		return render_to_response('bootstrap/index.html')
+
+
+"""
+This view constructs the API root for browsable API.
+"""
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'records/patient-data': reverse('patient-data-list', request=request, format=format),
+        'records/pid/history-data': reverse('history-data-list', request=request, format=format, kwargs={'pid':1})
+    })
+
+"""
+This view allow the user to use .list() or .retrieve() for history data based on the users pid.
+"""
+
+
+class HistoryDataList(generics.ListAPIView):
+	serializer_class = HistoryDataSerializer
+
+	def get_queryset(self):
+		pid = self.kwargs['pid']
+		return HistoryData.objects.filter(pid=pid)
+
+
+"""
+This view allow the user to use .list() or .retrieve() for patient data, as well as search by all fields.
+"""
+
+
+class PatientDataList(generics.ListAPIView):
+	serializer_class = PatientDataSerializer
+	queryset = PatientData.objects.all()
+	filter_backends = (filters.DjangoFilterBackend,)
+	filter_fields = ('pid', 'pubpid', 'ss', 'fname', 'lname', 'mname', 'dob', 'sex', 'status', 'email', 'street', 'postal_code', 'city', 'state', 'country_code')
+
+
+"""
+This view allow the user to view and modify MedicalHistory records.
+"""
+
+
+class MedicalHistoryViewSet(viewsets.ModelViewSet):
+	queryset = MedicalHistory.objects.all()
+	serializer_class = MedicalHistorySerializer
+
+
+"""
+This view allow the user to view and modify users.
+"""
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -19,42 +76,12 @@ class UserViewSet(viewsets.ModelViewSet):
 	serializer_class = UserSerializer
 
 
+"""
+This view allow the user to view and modify groups.
+"""
+
+
 class GroupViewSet(viewsets.ModelViewSet):
 	# API endpoint that allows groups to be viewed or edited
 	queryset = Group.objects.all()
 	serializer_class = GroupSerializer
-
-
-class PatientDataViewSet(viewsets.ModelViewSet):
-	# API endpoint for patient data to be viewed or added to
-	queryset = PatientData.objects.all()
-	serializer_class = PatientDataSerializer
-	filter_backends = (filters.DjangoFilterBackend,)
-	filter_fields = ('pid', 'pubpid', 'ss', 'fname', 'lname', 'mname', 'dob', 'sex', 'status', 'email', 'street', 'postal_code', 'city', 'state', 'country_code')
-
-
-class HistoryDataViewSet(viewsets.ModelViewSet):
-	# API endpoint for patient data to be viewed or added to
-	queryset = HistoryData.objects.all()
-	serializer_class = HistoryDataSerializer
-
-
-class MedicalHistoryViewSet(viewsets.ModelViewSet):
-	queryset = MedicalHistory.objects.all()
-	serializer_class = MedicalHistorySerializer
-
-
-class HistoryDataList(generics.ListAPIView):
-	serializer_class = HistoryDataSerializer
-	#authentication_classes = (authentication.TokenAuthentication)
-	#permission_classes = (permissions.IsAuthenticatedOrReadOnly)
-
-	def get_queryset(self):
-		pid = self.kwargs['pid']
-		return HistoryData.objects.filter(pid=pid)
-
-class PatientDataList(generics.ListAPIView):
-	serializer_class = PatientDataSerializer
-	queryset = PatientData.objects.all()
-	filter_backends = (filters.DjangoFilterBackend,)
-	filter_fields = ('pid', 'pubpid', 'ss', 'fname', 'lname', 'mname', 'dob', 'sex', 'status', 'email', 'street', 'postal_code', 'city', 'state', 'country_code')
