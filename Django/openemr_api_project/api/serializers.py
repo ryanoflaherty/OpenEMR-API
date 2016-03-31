415e4dc685df0ae0dff28a67c1db5cc9064db83d
ryanoflaherty <ryflare@bu.edu>
2016-03-31 18:29:00 -0400

    Non null serializer class to deal with Forms inheritance

--- Django/openemr_api_project/api/serializers.py
+++ Django/openemr_api_project/api/serializers.py
@@ -1,6 +1,38 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from models import PatientData, HistoryData, MedicalHistory, FormEncounter, Forms, Lists, Facility, FormRos, FormReviewofs, FormVitals
+from rest_framework.fields import SkipField
+from collections import OrderedDict
+
+
+class NonNullSerializer(serializers.ModelSerializer):
+    """
+    Use this to prevent null response fields from being serialized
+    """
+    def to_representation(self, instance):
+        """
+        Object instance -> Dict of primitive datatypes.
+        """
+        ret = OrderedDict()
+        fields = [field for field in self.fields.values() if not field.write_only]
+
+        for field in fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+
+            if attribute is not None:
+                represenation = field.to_representation(attribute)
+                if represenation is None:
+                    # Do not seralize empty objects
+                    continue
+                if isinstance(represenation, list) and not represenation:
+                   # Do not serialize empty lists
+                   continue
+                ret[field.field_name] = represenation
+
+        return ret
 
 
 # Users and Groups
@@ -53,7 +85,7 @@ class FormReviewofsSerializer(serializers.ModelSerializer):
 
 # Forms, List, HistoryData, PatientData
 ##################################################
-class FormsSerializer(serializers.ModelSerializer):
+class FormsSerializer(NonNullSerializer):
     id = serializers.IntegerField(read_only=True)
     date = serializers.DateTimeField()
     encounter = serializers.IntegerField()
