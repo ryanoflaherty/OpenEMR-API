564fbd55b3fe52e93b8ee3dc72b525d60cdfc4bf
ryanoflaherty <ryflare@bu.edu>
2016-04-11 20:05:42 -0400

    stub for POST to /records

--- Django/openemr_api_project/api/urls.py
+++ Django/openemr_api_project/api/urls.py
@@ -17,8 +17,9 @@ API Endpoints for app functionality
 """
 urlpatterns += [
     url(r'^records/$', views.CreateUpdateMedicalRecord.as_view(), name='medical-record-create-update'), # Create a new Medical History object
+    url(r'^test/$', views.create_visit, name='new-medical-record-create-update'),   # Create a new Medical History object
     url(r'^records/patient-data', views.PatientDataList.as_view(), name='patient-data-list'),   # Return a filtered list of Patient Data objects
-    url(r'^records/(?P<pubpid>[0-9]+)/visits', views.visits_list, name='visits-list'),  # Return a list of visits based on pid
+    url(r'^records/(?P<pubpid>[0-9]+)/visits', views.list_visits, name='visits-list'),  # Return a list of visits based on pid
     url(r'^test/(?P<pubpid>[0-9]+)/visits', views.VisitsHistoryList.as_view(), name='old-visits-list'), # Return a list of forms + hd based on pid
     #url(r'^users', views.UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='users'),
 ]
